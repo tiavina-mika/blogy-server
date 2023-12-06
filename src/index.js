cdfc244d0320241a8c75618713e4afd47ebda9b9
tiavina-mika <tiavinamika@gmail.com>
2023-12-06 16:05:23 +0300

    use express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("hello 4");
+const express = require("express")
+const cors = require("cors")
+
+const PORT = 8082;
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+  return res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(PORT, () => {
+  console.log("server running on port:" + PORT);
+})
