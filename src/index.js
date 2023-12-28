21c6d7d7227e886a8f442647a3e9d0edacb9b9a6
tiavina-mika <tiavinamika@gmail.com>
2023-12-28 16:25:54 +0300

    async database connection

--- src/index.js
+++ src/index.js
@@ -3,24 +3,29 @@ const cors = require("cors")
 const initDatabase = require("./config/database");
 const { createArticle, updateArticle, deleteArticle, getArticle, getArticles } = require("./controllers/article.controller");
 
-initDatabase();
-
 const PORT = 8082;
-const app = express();
 
-// -------- app config -------- //
-app.use(cors());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json())
-app.use(express.static('public'));
+const init = async () => {
+  await initDatabase();
+   
+  const app = express();
 
-// -------- endpoints -------- //
-app.post('/articles', createArticle)
-app.put('/articles/:id', updateArticle)
-app.delete('/articles/:id', deleteArticle)
-app.get('/articles/:id', getArticle)
-app.get('/articles', getArticles)
+  // -------- app config -------- //
+  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json())
+  app.use(express.static('public'));
 
-app.listen(PORT, () => {
-  console.log("server running on port:" + PORT);
-})
+  // -------- endpoints -------- //
+  app.post('/articles', createArticle)
+  app.put('/articles/:id', updateArticle)
+  app.delete('/articles/:id', deleteArticle)
+  app.get('/articles/:id', getArticle)
+  app.get('/articles', getArticles)
+
+  app.listen(PORT, () => {
+    console.log("server running on port:" + PORT);
+  })
+}
+
+init();
